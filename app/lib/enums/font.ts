bde6114d059e859bdf7d217274070eca46bb32b5
Sayeed Afridi <as.afridi563@gmail.com>
2025-01-01 16:05:24 +0600

    feat: added utilities and hooks

---
+++ app/lib/enums/font.ts
@@ -0,0 +1,9 @@
+export enum FontWeight {
+  ExtraLight = 'ExtraLight',
+  Light = 'Light',
+  Regular = 'Regular',
+  Medium = 'Medium',
+  SemiBold = 'SemiBold',
+  Bold = 'Bold',
+  ExtraBold = 'ExtraBold',
+}
